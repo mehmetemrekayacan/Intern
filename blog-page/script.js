43b2ecc9440e5cb3036d92f4d173c57a18c92fab
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-14 15:44:43 +0300

    make al lot of things

--- blog-page/script.js
+++ blog-page/script.js
@@ -1,12 +1,25 @@
 const hamburgerIcon = document.getElementById('hamburgerIcon');
+const menu = document.getElementById('menu');
+const blurBackground = document.getElementById('blur-background');
 let isOpen = false;
 
 hamburgerIcon.addEventListener('click', function () {
     const img = hamburgerIcon.querySelector('img');
     if (isOpen) {
         img.src = 'icons/hamburger-close.svg';  // Kapalı hali
+        menu.style.display = 'none';  // Menü gizlenir
+        blurBackground.style.display = 'none';  // Blur arka plan gizlenir
     } else {
         img.src = 'icons/hamburger-open.svg';  // Açık hali
+        menu.style.display = 'block';  // Menü gösterilir
+        blurBackground.style.display = 'block';  // Blur arka plan gösterilir
     }
     isOpen = !isOpen;
+});
+
+blurBackground.addEventListener('click', function () {
+    img.src = 'icons/hamburger-close.svg';  // Kapalı hali
+    menu.style.display = 'none';  // Menü gizlenir
+    blurBackground.style.display = 'none';  // Blur arka plan gizlenir
+    isOpen = false;
 });
