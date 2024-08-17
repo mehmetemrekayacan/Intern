cbf5f4557d9b983ae87c2f408eed7542d99c5162
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-17 11:20:40 +0300

    passanger count make with js

---
+++ javascript/passanger-count/index.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+}
