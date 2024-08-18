367c2cfddc06644ab6889ae4a303f102e7611e6c
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-18 23:13:58 +0300

    dropdown bar always on top

--- restaurant/script.js
+++ restaurant/script.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Menu items
     const menuItems = document.querySelectorAll(".nav-bar-a");
-    
-    // Dropdowns
     const dropdowns = {
         protein: document.getElementById("nav-bar-dropdown-protein"),
         carb: document.getElementById("nav-bar-dropdown-carb"),
@@ -10,30 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
         special: document.getElementById("nav-bar-dropdown-special"),
     };
 
-    // Function to hide all dropdowns
     function hideAllDropdowns() {
         Object.values(dropdowns).forEach(dropdown => dropdown.style.display = "none");
     }
 
-    // Add click event listener to each menu item
     menuItems.forEach(menuItem => {
         menuItem.addEventListener("click", function (event) {
-            event.preventDefault();  // Prevent default link behavior
-            
-            // Get the id of the clicked menu item
+            event.preventDefault();
             const id = this.id;
-            
-            // Toggle the corresponding dropdown visibility
-            if (dropdowns[id].style.display === "block") {
-                dropdowns[id].style.display = "none";
+            const dropdown = dropdowns[id];
+            const rect = this.getBoundingClientRect();
+
+            if (dropdown.style.display === "block") {
+                dropdown.style.display = "none";
             } else {
-                hideAllDropdowns(); // Hide all dropdowns first
-                dropdowns[id].style.display = "block"; // Show the clicked dropdown
+                hideAllDropdowns();
+                dropdown.style.display = "block";
+                dropdown.style.top = `${rect.bottom}px`; // Menü item'ının altından açılması için
+                dropdown.style.left = `${rect.left}px`; // Menü item'ının solundan hizalanması için
             }
         });
     });
 
-    // Hide dropdowns when clicking outside the menu
     document.addEventListener("click", function (event) {
         if (!event.target.closest(".nav-bar")) {
             hideAllDropdowns();
