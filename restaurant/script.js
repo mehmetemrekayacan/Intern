deba0e851bb998884551f6da29c397920536b92d
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-20 11:21:21 +0300

    add time

--- restaurant/script.js
+++ restaurant/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Toggle mobile menu
     const navBarItems = document.querySelectorAll(".nav-bar-a");
     const dropdowns = document.querySelectorAll(".nav-bar-dropdown");
-
     navBarItems.forEach(item => {
         item.addEventListener("click", function () {
             const targetDropdownId = "nav-bar-dropdown-" + this.id;
@@ -23,6 +23,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    // Update date and time in footer
+    function updateDateTime() {
+        const now = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+        const formattedDate = now.toLocaleDateString('en-US', options);
+        document.getElementById('footer-time').textContent = formattedDate;
+    }
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+
+    // Close dropdowns when clicking outside
     document.addEventListener("click", function (e) {
         if (!e.target.closest(".nav-bar-a")) {
             dropdowns.forEach(dropdown => {
