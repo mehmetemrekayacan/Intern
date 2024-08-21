4e2dbe49b48871f0d0afa59f657ff833c62f398d
mehmetemrekayacan <emremehmet32@hotmail.com>
2024-08-21 13:41:59 +0300

    empty input alert

--- javascript/even-odd-countries/index.js
+++ javascript/even-odd-countries/index.js
@@ -2,6 +2,10 @@ let countryCount = 0;
 
 function addCountry() {
     const input = document.getElementById('countryInput').value;
+    if (input.trim() === '') {
+        alert('Please enter a country name.');
+        return;
+    }
     countryCount++;
     const countryDiv = document.createElement('div');
     countryDiv.className = 'country ' + (countryCount % 2 === 0 ? 'even' : 'odd');
